551ef781d6c47964fbe91b8b98980be02b2154b0
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2023-01-18 15:41:07 -0500

    feat: ability for users to sign up

---
+++ app/Http/Controllers/API/Auth/RegisterController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\API\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+
+class RegisterController extends Controller
+{
+    /**
+     * Summary of store
+     * @param Request $request
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
+    public function store(Request $request) : Response
+    {
+        $request->validate([
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string',
+            'firstName' => 'required|string',
+            'lastName' => 'required|string',
+            'type' => ['required', 'string', Rule::in(['client', 'staff'])],
+            'isAdmin' => 'required|boolean'
+        ]);
+
+        // Hash the user password before saving to the database
+        $request['password'] = Hash::make($request['password']);
+
+        $user = User::create($request->all());
+
+        $token = $user->createToken('bankapi')->plainTextToken;
+
+        return response([
+            'status' => "201",
+            'data' => array_merge($user->toArray(), ['token' => $token])
+        ], 201);
+    }
+}
